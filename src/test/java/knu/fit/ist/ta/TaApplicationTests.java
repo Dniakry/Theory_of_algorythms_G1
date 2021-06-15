a8e8f93dd44d9ca35c405e6f786a6f4507466b29
Dniakry <yura100sqwe@gmail.com>
2021-06-15 14:50:02 +0300

    lab3

--- src/test/java/knu/fit/ist/ta/TaApplicationTests.java
+++ src/test/java/knu/fit/ist/ta/TaApplicationTests.java
@@ -3,6 +3,8 @@ package knu.fit.ist.ta;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import knu.fit.ist.ta.laba_3.Lab3;
+
 
 @SpringBootTest
 class TaApplicationTests {
@@ -17,4 +19,13 @@ class TaApplicationTests {
         assertEquals(0.02f, 0.025f, 0.01f);
         }
 
+        @Test
+    void Laba3Test()
+    {
+        Lab3 lab3 = new Lab3(-5);
+        assertEquals(20, lab3.GetArrayLength());
+        lab3 = new Lab3(10);
+        lab3.InitArray();
+        assertEquals(10, lab3.GetArrayLength());
+    }
 }
